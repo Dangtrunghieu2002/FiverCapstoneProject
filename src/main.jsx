2ca6ac6b68549db1833e6e2f88c4cb9a4fd7f5d1
Dang Trung Hieu <dangtrunghieu0904hcm@gmail.com>
2024-08-31 19:56:53 +0700

    setup react template

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/configStore.js'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <BrowserRouter>
         <App />
